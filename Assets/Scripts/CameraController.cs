60a84f76328415ed5a24f9399beba8dd666c10d8
scolia <scolia@qq.com>
2017-06-01 17:46:41 +0800

    init

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject m_Player;
+
+    private Vector3 m_Offset;
+
+	// Use this for initialization
+	void Start () {
+        // 由摄像机指向游戏物体的向量
+        m_Offset = m_Player.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = m_Player.transform.position - m_Offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text m_CountText;
+    public Text m_WinText;
+    public GameObject PickUps;
+
+    private Rigidbody m_Rigidbody;
+    private int m_Count = 0;
+    private int m_PickUpNums = 0;
+
+	// Use this for initialization
+	void Start () {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_PickUpNums = PickUps.transform.childCount;
+	}
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
+
+        m_Rigidbody.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("PickUp"))
+        {
+            Destroy(other.gameObject);
+            m_Count++;
+            SetCountText();
+            DisplayWinText();
+        }
+    }
+
+    void SetCountText()
+    {
+        m_CountText.text = "Count: " + m_Count.ToString();
+    }
+
+    void DisplayWinText()
+    {
+        if (m_Count >= m_PickUpNums)
+        {
+            m_WinText.gameObject.SetActive(true);
+        }
+    }
+}
