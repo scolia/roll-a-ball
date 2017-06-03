849422233fc564b66c05515f7dddf6dff8925458
scolia <scolia@qq.com>
2017-06-04 00:36:09 +0800

    添加重置按钮

---
+++ Assets/Scripts/ResetScene.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ResetScene : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //重载场景
+        SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+    }
+}
