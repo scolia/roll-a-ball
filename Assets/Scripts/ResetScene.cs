4a812ee6a33980d14b623bf9204f8eef8d19d5b8
scolia <scolia@qq.com>
2017-06-04 14:01:03 +0800

    添加暂停按钮的音效

--- Assets/Scripts/ResetScene.cs
+++ Assets/Scripts/ResetScene.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class ResetScene : MonoBehaviour, IPointerClickHandler
+[RequireComponent(typeof(AudioSource))] // 要求附着的对象必须要有AudioSource组件
+public class ResetScene : MonoBehaviour
 {
-    public void OnPointerClick(PointerEventData eventData)
+    private AudioSource m_AudioSource;
+
+    private void Start()
     {
-        //重载场景
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    public void Reset()
+    {
+        //播放音效并重置场景
+        m_AudioSource.Play();
+        // 延迟等待声音播放完毕后再重置场景, 若不延迟, 就会导致声音无法播放
+        StartCoroutine(DelayedRest(m_AudioSource.clip.length)); 
+    }
+
+    IEnumerator DelayedRest(float time)
+    {
+        yield return new WaitForSeconds(time);
         SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
     }
 }
